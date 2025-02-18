4db9ac47ad2c692caf907f2fee731b46d5b9a613
Gabriel Guerra <gfranklings@gmail.com>
2025-02-18 05:57:00 -0400

    feat(ags/dock): Make click work

--- cfg/ags/widget/Dock/dock.tsx
+++ cfg/ags/widget/Dock/dock.tsx
@@ -8,6 +8,16 @@ import AstalHyprland from "gi://AstalHyprland?version=0.1";
 function Application({ app }: { app: AstalApps.Application }) {
   const hyprland = AstalHyprland.get_default();
 
+  const hyprClient = bind(hyprland, "clients").as((clients) => {
+    return clients.find((client) => {
+      const wmClass = client.get_class();
+      return (
+        app.get_entry() === wmClass + ".desktop" ||
+        app.get_wm_class() === wmClass
+      );
+    });
+  });
+
   const focusedClient = bind(hyprland, "focusedClient").as((client) => {
     if (!Boolean(client)) return false;
     const wmClass = client.get_class();
@@ -16,8 +26,12 @@ function Application({ app }: { app: AstalApps.Application }) {
     );
   });
 
+  const handleClick = () => {
+    hyprClient.get()?.focus();
+  };
+
   return (
-    <button className="appButton">
+    <button className="appButton" onClicked={handleClick}>
       <box vertical expand spacing={4}>
         <icon icon={app.iconName} />
         <box
