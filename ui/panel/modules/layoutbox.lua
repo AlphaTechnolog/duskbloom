d5689eca70b367d5ab153c348d8d3d7368160543
Gabriel Guerra <gfranklings@gmail.com>
2025-03-25 18:06:30 +0000

    chore: Run fmt

--- ui/panel/modules/layoutbox.lua
+++ ui/panel/modules/layoutbox.lua
@@ -25,14 +25,22 @@ function _layoutbox:render()
         screen = self.s,
         forced_width = dpi(18),
         forced_height = dpi(18),
-      }
+      },
     },
 
     buttons = {
-      awful.button({}, 1, function () awful.layout.inc(1) end),
-      awful.button({}, 3, function () awful.layout.inc(-1) end),
-      awful.button({}, 4, function () awful.layout.inc(1) end),
-      awful.button({}, 5, function () awful.layout.inc(-1) end),
+      awful.button({}, 1, function()
+        awful.layout.inc(1)
+      end),
+      awful.button({}, 3, function()
+        awful.layout.inc(-1)
+      end),
+      awful.button({}, 4, function()
+        awful.layout.inc(1)
+      end),
+      awful.button({}, 5, function()
+        awful.layout.inc(-1)
+      end),
     },
   }))
 
