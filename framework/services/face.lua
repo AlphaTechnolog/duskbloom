9cdea1c8e62a2c7e311c13a2899a66f7093beb1a
Gabriel Guerra <gfranklings@gmail.com>
2025-06-25 17:16:22 -0400

    chore: Change code style

--- framework/services/face.lua
+++ framework/services/face.lua
@@ -1,22 +1,21 @@
-local gfs = require("gears.filesystem")
-local utils = require("framework.utils")()
-local oop = require("framework.oop")
+local gfs = require('gears.filesystem')
+local utils = require('framework.utils')()
+local oop = require('framework.oop')
 
 local face = {}
 
 function face:fetch()
-	local default_face_path = gfs.get_configuration_dir()
-		.. "assets/icons/face.default.png"
+   local default_face_path = gfs.get_configuration_dir() .. 'assets/icons/face.default.png'
 
-	local pos = utils:map({ "jpg", "png", "svg" }, ipairs, function(_, item)
-		return os.getenv("HOME") .. "/.face." .. item
-	end)
+   local pos = utils:map({ 'jpg', 'png', 'svg' }, ipairs, function(_, item)
+      return os.getenv('HOME') .. '/.face.' .. item
+   end)
 
-	local path = utils:find(pos, ipairs, function(_, path)
-		return gfs.file_readable(path)
-	end) or default_face_path
+   local path = utils:find(pos, ipairs, function(_, path)
+      return gfs.file_readable(path)
+   end) or default_face_path
 
-	return path
+   return path
 end
 
 return oop(face)
--- ui/guards/hoverable.lua
+++ ui/guards/hoverable.lua
@@ -1,42 +1,40 @@
-local animation = require("framework.animation")
-local color = require("framework.color")
-local beautiful = require("beautiful")
+local animation = require('framework.animation')
+local color = require('framework.color')
+local beautiful = require('beautiful')
 
 return function(widget)
-	function widget:setup_hover(opts)
-		self.opts = opts
-		self.animation = animation:new({
-			duration = 0.25,
-			easing = animation.easing.inOutQuad,
-			pos = color.hex_to_rgba(
-				opts.colors.normal or beautiful.colors.background
-			),
-			update = function(_, pos)
-				local hex = color.rgba_to_hex(pos)
-				widget:emit_signal("animation:hex-change", hex)
-				widget.bg = hex
-			end,
-		})
+   function widget:setup_hover(opts)
+      self.opts = opts
+      self.animation = animation:new({
+         duration = 0.25,
+         easing = animation.easing.inOutQuad,
+         pos = color.hex_to_rgba(opts.colors.normal or beautiful.colors.background),
+         update = function(_, pos)
+            local hex = color.rgba_to_hex(pos)
+            widget:emit_signal('animation:hex-change', hex)
+            widget.bg = hex
+         end,
+      })
 
-		self:subscribe_hover()
-	end
+      self:subscribe_hover()
+   end
 
-	function widget:use_color(new_color)
-		if not self.animation then
-			error("[widget:hoverable] no animation yet, call setup() first")
-		end
+   function widget:use_color(new_color)
+      if not self.animation then
+         error('[widget:hoverable] no animation yet, call setup() first')
+      end
 
-		self.animation:set({ target = color.hex_to_rgba(new_color) })
-	end
+      self.animation:set({ target = color.hex_to_rgba(new_color) })
+   end
 
-	function widget:subscribe_hover()
-		self:connect_signal("mouse::enter", function()
-			self:use_color(self.opts.colors.hovered)
-		end)
-		self:connect_signal("mouse::leave", function()
-			self:use_color(self.opts.colors.normal)
-		end)
-	end
+   function widget:subscribe_hover()
+      self:connect_signal('mouse::enter', function()
+         self:use_color(self.opts.colors.hovered)
+      end)
+      self:connect_signal('mouse::leave', function()
+         self:use_color(self.opts.colors.normal)
+      end)
+   end
 
-	return widget
+   return widget
 end
